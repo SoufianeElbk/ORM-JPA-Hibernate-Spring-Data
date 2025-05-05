286baf983c3bee877ed5e2784295cdd12b62b7e9
Soufiane ELBOUBKARI <elboubkarisoufiane03@gmail.com>
2025-05-05 01:19:30 +0100

    Create Patient entity

---
+++ src/main/java/ma/enset/ap2/entities/Patient.java
@@ -0,0 +1,26 @@
+package ma.enset.ap2.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.util.Date;
+
+@Entity
+public class Patient {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private Date dateOfBirth;
+    private boolean sickness;
+    private int score;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+}
